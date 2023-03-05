52376494fba05ba2aab971785bb7159de72d31c2
melops-hh <melops@protonmail.com>
2023-03-05 16:34:37 +0100

    update harpoon keybindings

--- config/nvim/after/plugin/harpoon.lua
+++ config/nvim/after/plugin/harpoon.lua
@@ -12,8 +12,8 @@ end
 local mark = require('harpoon.mark')
 local ui = require('harpoon.ui')
 
-vim.keymap.set('n', '<C-e>', ui.toggle_quick_menu)
-vim.keymap.set('n', '<leader>a', mark.add_file)
+vim.keymap.set('n', '<leader>fm', ui.toggle_quick_menu)
+vim.keymap.set('n', '<leader>fa', mark.add_file)
 
 vim.keymap.set('n', '<leader>1', function() ui.nav_file(1)end)
 vim.keymap.set('n', '<leader>2', function() ui.nav_file(2)end)
