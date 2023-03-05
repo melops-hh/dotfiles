0e535c71f9067b36a57e4b5f1c5da0b67cc06778
melops-hh <melops@protonmail.com>
2023-03-05 16:32:20 +0100

    move and update telescope keybindings

--- config/nvim/after/plugin/telescope.lua
+++ config/nvim/after/plugin/telescope.lua
@@ -55,3 +55,20 @@ vim.keymap.set('n', '<leader>sd', require('telescope.builtin').diagnostics, {
 vim.keymap.set('n', '<c-p>', require('telescope.builtin').git_files, {
     desc = '[S]earch [G]it [F]iles'
 })
+-- See `:help telescope.builtin`
+vim.keymap.set("n", "<leader>?", require("telescope.builtin").oldfiles, { desc = "[?] Find recently opened files" })
+vim.keymap.set("n", "<leader><space>", require("telescope.builtin").buffers, { desc = "[ ] Find existing buffers" })
+vim.keymap.set("n", "<leader>/", function()
+    -- You can pass additional configuration to telescope to change theme, layout, etc.
+    require("telescope.builtin").current_buffer_fuzzy_find(require("telescope.themes").get_dropdown {
+        winblend = 10,
+        previewer = false
+    })
+end, { desc = "[/] Fuzzily search in current buffer]" })
+
+vim.keymap.set("n", "<leader>ff", require("telescope.builtin").find_files, { desc = "[S]earch [F]iles" })
+vim.keymap.set("n", "<leader>fh", require("telescope.builtin").help_tags, { desc = "[S]earch [H]elp" })
+-- vim.keymap.set("n", "<leader>sw", require("telescope.builtin").grep_string, { desc = "[S]earch current [W]ord" })
+vim.keymap.set("n", "<leader>fw", require("telescope.builtin").live_grep, { desc = "[S]earch by [G]rep" })
+vim.keymap.set("n", "<leader>fd", require("telescope.builtin").diagnostics, { desc = "[S]earch [D]iagnostics" })
+vim.keymap.set("n", "<c-p>", require("telescope.builtin").git_files , { desc = "[S]earch [G]it [F]iles" })
--- config/nvim/lua/melops/keybindings.lua
+++ config/nvim/lua/melops/keybindings.lua
@@ -25,24 +25,6 @@ map({"n", "v"}, "<leader>p", '"_dP', { noremap = true })
 map({"v"}, "J", ":m '>+1<CR>gv=gv")
 map({"v"}, "K", ":m '<-2<CR>gv=gv")
 
--- See `:help telescope.builtin`
-map("n", "<leader>?", require("telescope.builtin").oldfiles, { desc = "[?] Find recently opened files" })
-map("n", "<leader><space>", require("telescope.builtin").buffers, { desc = "[ ] Find existing buffers" })
-map("n", "<leader>/", function()
-    -- You can pass additional configuration to telescope to change theme, layout, etc.
-    require("telescope.builtin").current_buffer_fuzzy_find(require("telescope.themes").get_dropdown {
-        winblend = 10,
-        previewer = false
-    })
-end, { desc = "[/] Fuzzily search in current buffer]" })
-
-map("n", "<leader>sf", require("telescope.builtin").find_files, { desc = "[S]earch [F]iles" })
-map("n", "<leader>sh", require("telescope.builtin").help_tags, { desc = "[S]earch [H]elp" })
-map("n", "<leader>sw", require("telescope.builtin").grep_string, { desc = "[S]earch current [W]ord" })
-map("n", "<leader>sg", require("telescope.builtin").live_grep, { desc = "[S]earch by [G]rep" })
-map("n", "<leader>sd", require("telescope.builtin").diagnostics, { desc = "[S]earch [D]iagnostics" })
-map("n", "<c-p>", require("telescope.builtin").git_files, { desc = "[S]earch [G]it [F]iles" })
-
 -- Diagnostic keymaps
 map("n", "[d", vim.diagnostic.goto_prev)
 map("n", "]d", vim.diagnostic.goto_next)
