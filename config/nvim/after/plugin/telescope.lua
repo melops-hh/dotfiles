e13df632af31cf4e74d132f4ad73af72e0c3e00b
melops-hh <melops@protonmail.com>
2023-04-20 19:08:35 +0300

    remove duplicated keymap

--- config/nvim/after/plugin/telescope.lua
+++ config/nvim/after/plugin/telescope.lua
@@ -55,16 +55,6 @@ vim.keymap.set('n', '<leader>?', require('telescope.builtin').oldfiles, {
 vim.keymap.set('n', '<leader><space>', require('telescope.builtin').buffers, {
     desc = '[ ] Find existing buffers'
 })
-vim.keymap.set('n', '<leader>/', function()
-    -- You can pass additional configuration to telescope to change theme, layout, etc.
-    require('telescope.builtin').current_buffer_fuzzy_find(require('telescope.themes').get_dropdown {
-        winblend = 10,
-        previewer = false
-    })
-end, {
-    desc = '[/] Fuzzily search in current buffer]'
-})
-
 vim.keymap.set('n', '<leader>sf', require('telescope.builtin').find_files, {
     desc = '[S]earch [F]iles'
 })
