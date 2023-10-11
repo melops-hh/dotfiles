27a1cc603ffb0d96881581f343b58a5f9a3acd84
melops-hh <melops@protonmail.com>
2023-10-11 15:30:40 +0200

    migrate to ident-blankline v3

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -141,12 +141,10 @@ require('lazy').setup({
   {
     -- Add indentation guides even on blank lines
     'lukas-reineke/indent-blankline.nvim',
+    main = "ibl",
     -- Enable `lukas-reineke/indent-blankline.nvim`
     -- See `:help indent_blankline.txt`
-    opts = {
-      char = '┊',
-      show_trailing_blankline_indent = false,
-    },
+    opts = {},
   },
 
   -- "cl" to comment visual regions/lines
