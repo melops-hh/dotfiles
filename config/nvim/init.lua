79639016c47a90310d61c436c99d3f2448ce12db
melops-hh <melops@protonmail.com>
2024-09-21 17:06:59 +0200

    nvim: set todo-comments signs to true

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -129,7 +129,7 @@ require('lazy').setup({
   -- },
 
   -- Highlight todo, notes, etc in comments
-  { 'folke/todo-comments.nvim', event = 'VimEnter', dependencies = { 'nvim-lua/plenary.nvim' }, opts = { signs = false } },
+  { 'folke/todo-comments.nvim', event = 'VimEnter', dependencies = { 'nvim-lua/plenary.nvim' }, opts = { signs = true } },
 
   { -- Collection of various small independent plugins/modules
     'echasnovski/mini.nvim',
