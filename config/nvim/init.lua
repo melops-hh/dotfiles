f4b43a5e8f959b8186dd3b75ff2a152b1949ee02
melops-hh <melops@protonmail.com>
2024-09-21 17:10:06 +0200

    nvim: add trouble keybindings

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -228,7 +228,8 @@ require('lazy').setup({
   },
   {
     'folke/trouble.nvim',
-    branch = 'dev', -- IMPORTANT!
+    opts = {}, -- for default options, refer to the configuration section for custom setup.
+    cmd = 'Trouble',
     keys = {
       {
         '<leader>xx',
@@ -240,8 +241,27 @@ require('lazy').setup({
         '<cmd>Trouble diagnostics toggle filter.buf=0<cr>',
         desc = 'Buffer Diagnostics (Trouble)',
       },
+      {
+        '<leader>cs',
+        '<cmd>Trouble symbols toggle focus=false<cr>',
+        desc = 'Symbols (Trouble)',
+      },
+      {
+        '<leader>cl',
+        '<cmd>Trouble lsp toggle focus=false win.position=right<cr>',
+        desc = 'LSP Definitions / references / ... (Trouble)',
+      },
+      {
+        '<leader>xL',
+        '<cmd>Trouble loclist toggle<cr>',
+        desc = 'Location List (Trouble)',
+      },
+      {
+        '<leader>xQ',
+        '<cmd>Trouble qflist toggle<cr>',
+        desc = 'Quickfix List (Trouble)',
+      },
     },
-    opts = {}, -- for default options, refer to the configuration section for custom setup.
   },
   {
     'github/copilot.vim',
