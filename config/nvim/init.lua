779cc0e310a11e14101ddc6df2ffc663ac58261b
melops-hh <melops@protonmail.com>
2024-09-21 17:03:58 +0200

    nvim: update which-key settings

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -32,7 +32,7 @@ require('lazy').setup({
   --    require('Comment').setup({})
 
   -- "gc" to comment visual regions/lines
-  { 'numToStr/Comment.nvim', opts = {} },
+  -- { 'numToStr/Comment.nvim', opts = {} },
 
   -- NOTE: Plugins can also be configured to run Lua code when they are loaded.
   --
@@ -51,19 +51,29 @@ require('lazy').setup({
 
   { -- Useful plugin to show you pending keybinds.
     'folke/which-key.nvim',
-    event = 'VimEnter', -- Sets the loading event to 'VimEnter'
-    config = function() -- This is the function that runs, AFTER loading
-      require('which-key').setup()
-
-      -- Document existing key chains
-      require('which-key').register {
-        ['<leader>c'] = { name = '[C]ode', _ = 'which_key_ignore' },
-        ['<leader>d'] = { name = '[D]ocument', _ = 'which_key_ignore' },
-        ['<leader>r'] = { name = '[R]ename', _ = 'which_key_ignore' },
-        ['<leader>s'] = { name = '[S]earch', _ = 'which_key_ignore' },
-        ['<leader>w'] = { name = '[W]orkspace', _ = 'which_key_ignore' },
-      }
-    end,
+    event = 'VeryLazy', -- Sets the loading event to 'VimEnter'
+    opts = {},
+    keys = {
+      {
+        '<leader>?',
+        function()
+          require('which-key').show { global = false }
+        end,
+        desc = 'Buffer Local Keymaps (which-key)',
+      },
+    },
+    -- config = function() -- This is the function that runs, AFTER loading
+    --   require('which-key').setup()
+    --
+    --   -- Document existing key chains
+    --   require('which-key').register {
+    --     ['<leader>c'] = { name = '[C]ode', _ = 'which_key_ignore' },
+    --     ['<leader>d'] = { name = '[D]ocument', _ = 'which_key_ignore' },
+    --     ['<leader>r'] = { name = '[R]ename', _ = 'which_key_ignore' },
+    --     ['<leader>s'] = { name = '[S]earch', _ = 'which_key_ignore' },
+    --     ['<leader>w'] = { name = '[W]orkspace', _ = 'which_key_ignore' },
+    --   }
+    -- end,
   },
 
   { -- Autoformat
@@ -88,6 +98,7 @@ require('lazy').setup({
         -- You can use a sub-list to tell conform to run *until* a formatter
         -- is found.
         javascript = { { 'prettierd', 'prettier' } },
+        typescript = { 'prettier' },
       },
     },
   },
