8225e5684ed5ed9eadbbc80e64a16b282163c49a
melops-hh <melops@protonmail.com>
2024-09-24 17:23:31 +0200

    chore(nvim): clean up keymaps

--- config/nvim/lua/config/keymaps.lua
+++ config/nvim/lua/config/keymaps.lua
@@ -5,14 +5,6 @@
 vim.opt.hlsearch = true
 vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
 
--- Exit terminal mode in the builtin terminal with a shortcut that is a bit easier
--- for people to discover. Otherwise, you normally need to press <C-\><C-n>, which
--- is not what someone will guess without a bit more experience.
---
--- This won't work in all terminal emulators/tmux/etc. Try your own mapping
--- or just use <C-\><C-n> to exit terminal mode
--- vim.keymap.set('t', '<Esc><Esc>', '<C-\\><C-n>', { desc = 'Exit terminal mode' })
-
 -- Keybinds to make split navigation easier.
 --  Use CTRL+<hjkl> to switch between windows
 --  See `:help wincmd` for a list of all window commands
@@ -20,6 +12,10 @@ vim.keymap.set('n', '<C-h>', '<C-w><C-h>', { desc = 'Move focus to the left wind
 vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right window' })
 vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
 vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
+
+-- center on next/prev
+vim.keymap.set('n', 'n', 'nzzzv', { remap = false, desc = 'Next search result' })
+vim.keymap.set('n', 'N', 'Nzzzv', { remap = false, desc = 'Prev search result' })
 
 -- map kj to ESC
 vim.keymap.set('i', 'kj', '<ESC>', { silent = true })
@@ -35,12 +31,18 @@ vim.keymap.set({ 'n', 'v' }, 'X', '"_d', { noremap = true })
 -- paste without registering word
 vim.keymap.set({ 'n', 'v' }, '<leader>p', '"_dP', { noremap = true })
 
+-- move indentation
+vim.keymap.set('v', '<', '<gv', { remap = false, desc = 'Move indentation to left' })
+vim.keymap.set('v', '>', '>gv', { remap = false, desc = 'Move indentation to right' })
+
+-- Quickfixlist
+vim.keymap.set('n', 'ql', '<cmd>copen<cr>', { remap = false, desc = 'Open quickfixlist' })
+vim.keymap.set('n', 'qn', '<cmd>cnewer<cr>', { remap = false, desc = 'Next quickfixlist in history' })
+vim.keymap.set('n', 'qp', '<cmd>colder<cr>', { remap = false, desc = 'Prev quickfixlist in history' })
+
 -- moving blocks with automatically indenting
 vim.keymap.set({ 'v' }, 'J', ":m '>+1<CR>gv=gv")
 vim.keymap.set({ 'v' }, 'K', ":m '<-2<CR>gv=gv")
-
--- mini.files
-vim.keymap.set('n', '<leader>mf', ':lua MiniFiles.open()<CR>', { noremap = true })
 
 -- Search and replace on the current word under the cursor
 vim.keymap.set('n', '<leader>s', [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
