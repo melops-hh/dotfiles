4607cd6ea8b9d293f4eb21facd2e7a60f5d6aec7
melops-hh <melops@protonmail.com>
2024-12-10 12:48:27 +0100

    remove unused keymaps

--- config/nvim/lua/config/keymaps.lua
+++ config/nvim/lua/config/keymaps.lua
@@ -1,17 +1,8 @@
--- [[ Basic Keymaps ]]
---  See `:help vim.keymap.set()`
+vim.keymap.set('n', '<space><space>x', '<cmd>source %<CR>')
 
 -- Set highlight on search, but clear on pressing <Esc> in normal mode
 vim.opt.hlsearch = true
 vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
-
--- Keybinds to make split navigation easier.
---  Use CTRL+<hjkl> to switch between windows
---  See `:help wincmd` for a list of all window commands
-vim.keymap.set('n', '<C-h>', '<C-w><C-h>', { desc = 'Move focus to the left window' })
-vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right window' })
-vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
-vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
 
 -- center on next/prev
 vim.keymap.set('n', 'n', 'nzzzv', { remap = false, desc = 'Next search result' })
@@ -34,11 +25,6 @@ vim.keymap.set({ 'n', 'v' }, '<leader>p', '"_dP', { noremap = true })
 -- move indentation
 vim.keymap.set('v', '<', '<gv', { remap = false, desc = 'Move indentation to left' })
 vim.keymap.set('v', '>', '>gv', { remap = false, desc = 'Move indentation to right' })
-
--- Quickfixlist
-vim.keymap.set('n', 'ql', '<cmd>copen<cr>', { remap = false, desc = 'Open quickfixlist' })
-vim.keymap.set('n', 'qn', '<cmd>cnewer<cr>', { remap = false, desc = 'Next quickfixlist in history' })
-vim.keymap.set('n', 'qp', '<cmd>colder<cr>', { remap = false, desc = 'Prev quickfixlist in history' })
 
 -- moving blocks with automatically indenting
 vim.keymap.set({ 'v' }, 'J', ":m '>+1<CR>gv=gv")
