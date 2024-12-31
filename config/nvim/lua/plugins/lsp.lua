ccbd4c6a080f6c77857e534b3798438355cf2b44
melops-hh <melops@protonmail.com>
2024-12-31 17:41:10 +0100

    feat(swift): add sourcekit

--- config/nvim/lua/plugins/lsp.lua
+++ config/nvim/lua/plugins/lsp.lua
@@ -34,6 +34,9 @@ return {
       },
     },
     config = function()
+      local lspconfig = require 'lspconfig'
+      lspconfig.sourcekit.setup {}
+
       vim.api.nvim_create_autocmd('LspAttach', {
         group = vim.api.nvim_create_augroup('kickstart-lsp-attach', { clear = true }),
         callback = function(event)
