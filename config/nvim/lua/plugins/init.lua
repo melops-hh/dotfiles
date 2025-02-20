68d4b72c716856aa10562d5f3df2c2a504d4a9f3
melops-hh <melops@protonmail.com>
2025-02-20 11:13:50 +0100

    clean up what snacks replaced
    - part of fugitiv
    - indent-blankline
    - lazygit
    - zen-mode
    - telescope

--- config/nvim/lua/plugins/init.lua
+++ config/nvim/lua/plugins/init.lua
@@ -36,8 +36,8 @@ return {
       end,
       formatters_by_ft = {
         lua = { 'stylua' },
-        python = { 'isort', 'black' },
-        javascript = { { 'prettierd', 'prettier' } },
+        -- python = { 'isort', 'black' },
+        javascript = { 'prettierd', 'prettier' },
         typescript = { 'prettier' },
       },
     },
@@ -48,15 +48,6 @@ return {
     event = 'VeryLazy',
     dependencies = { 'nvim-lua/plenary.nvim' },
     opts = { signs = true },
-  },
-
-  { -- Add indentation guides even on blank lines
-    'lukas-reineke/indent-blankline.nvim',
-    event = 'VeryLazy',
-    main = 'ibl',
-    ---@module "ibl"
-    ---@type ibl.config
-    opts = {},
   },
 
   {
@@ -70,66 +61,6 @@ return {
   },
 
   {
-    'voldikss/vim-floaterm',
-    keys = {
-      { '<leader>lz', '<cmd> FloatermNew --width=1.00 --height=1.00 lazygit<CR>', mode = 'n', desc = 'Toggle lazygit' },
-    },
-  },
-
-  {
-    'folke/zen-mode.nvim',
-    opts = {
-      plugins = {
-        options = {
-          enabled = true,
-          laststatus = 3,
-        },
-      },
-    },
-    keys = {
-      { '<leader>zz', '<cmd>ZenMode<CR>', desc = 'Toggle ZenMode' },
-    },
-  },
-
-  {
-    'folke/trouble.nvim',
-    opts = {},
-    cmd = 'Trouble',
-    keys = {
-      {
-        '<leader>xx',
-        '<cmd>Trouble diagnostics toggle<cr>',
-        desc = 'Diagnostics (Trouble)',
-      },
-      {
-        '<leader>xX',
-        '<cmd>Trouble diagnostics toggle filter.buf=0<cr>',
-        desc = 'Buffer Diagnostics (Trouble)',
-      },
-      {
-        '<leader>cs',
-        '<cmd>Trouble symbols toggle focus=false<cr>',
-        desc = 'Symbols (Trouble)',
-      },
-      {
-        '<leader>cl',
-        '<cmd>Trouble lsp toggle focus=false win.position=right<cr>',
-        desc = 'LSP Definitions / references / ... (Trouble)',
-      },
-      {
-        '<leader>xL',
-        '<cmd>Trouble loclist toggle<cr>',
-        desc = 'Location List (Trouble)',
-      },
-      {
-        '<leader>xQ',
-        '<cmd>Trouble qflist toggle<cr>',
-        desc = 'Quickfix List (Trouble)',
-      },
-    },
-  },
-
-  {
     'zbirenbaum/copilot.lua',
     cmd = 'Copilot',
     event = 'InsertEnter',
@@ -139,60 +70,6 @@ return {
         panel = { enabled = false },
       }
     end,
-  },
-
-  {
-    'folke/noice.nvim',
-    event = 'VeryLazy',
-    opts = {},
-    dependencies = {
-      -- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
-      'MunifTanjim/nui.nvim',
-      -- OPTIONAL:
-      --   `nvim-notify` is only needed, if you want to use the notification view.
-      --   If not available, we use `mini` as the fallback
-      'rcarriga/nvim-notify',
-    },
-    keys = {
-      {
-        '<leader>snl',
-        function()
-          require('noice').cmd 'last'
-        end,
-        desc = 'Noice Last Message',
-      },
-      {
-        '<leader>snh',
-        function()
-          require('noice').cmd 'history'
-        end,
-        desc = 'Noice History',
-      },
-      {
-        '<leader>sna',
-        function()
-          require('noice').cmd 'all'
-        end,
-        desc = 'Noice All',
-      },
-      {
-        '<leader>snd',
-        function()
-          require('noice').cmd 'dismiss'
-        end,
-        desc = 'Dismiss All',
-      },
-    },
-  },
-
-  {
-    'nvim-tree/nvim-tree.lua',
-    event = 'VeryLazy',
-    opts = {},
-    keys = {
-      { '<C-b>', '<cmd>NvimTreeToggle<CR>', desc = 'Toggle NvimTree' },
-      { '<C-n>', '<cmd>NvimTreeFindFile<CR>', desc = 'Find file in NvimTree' },
-    },
   },
 
   {
